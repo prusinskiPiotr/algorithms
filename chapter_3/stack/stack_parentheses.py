f17502cca4d5df3d0168d222e27447862b07f00c
Piotr <prusinski.pio@gmail.com>
2018-07-18 17:39:31 +0200

    add matches function to check for different parentheses

--- chapter_3/stack/stack_parentheses.py
+++ chapter_3/stack/stack_parentheses.py
@@ -7,20 +7,28 @@ def parChecker(symbolString):
     index = 0
     while index < len(symbolString) and balanced:
         symbol = symbolString[index]
-        if symbol == "(":
+        if symbol in "([{":
             s.push(symbol)
         else:
             if s.isEmpty():
                 balanced = False
             else:
-                s.pop()
-
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
         index = index + 1
-
     if balanced and s.isEmpty():
         return True
     else:
         return False
 
+def matches(open, close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
+
+
 print(parChecker('((()))'))
 print(parChecker('(()'))
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
