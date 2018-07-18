6c7d5c867c8e8c53b6635a765d510fd6cf039d26
Piotr <prusinski.pio@gmail.com>
2018-07-18 18:20:23 +0200

    implement a decimal to binary converter using stack

---
+++ chapter_3/stack/stack_decimal_to_binary.py
@@ -0,0 +1,18 @@
+from stack import Stack
+
+def divideByTwo(decNumber):
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remstack.push(rem)
+        decNumber = decNumber // 2
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString = binString + str(remstack.pop())
+
+    return binString
+
+print(divideByTwo(233))
+
