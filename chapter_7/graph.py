ecc59052e284fea7ef1db5fe286dbeccba558324
Piotr <prusinski.pio@gmail.com>
2018-11-20 12:16:54 +0100

    text graph implementation, fix typos

--- chapter_7/graph.py
+++ chapter_7/graph.py
@@ -44,10 +44,30 @@ class Graph:
             nv = self.addVertex(f)
         if t not in self.vertList:
             nv = self.addVertex(t)
-        self.vertList[f].addNeightbour(self.vertList[t], cost)
+        self.vertList[f].addNeighbour(self.vertList[t], cost)
 
     def getVertices(self):
         return self.vertList.keys()
 
     def __iter__(self):
         return iter(self.vertList.values())
+
+
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+
+
+for v in g:
+    for w in v.getConnections():
+        print("( %s , %s )" % (v.getId(), w.getId()))
