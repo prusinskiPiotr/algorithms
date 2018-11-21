d32a4f628a85b7079a497225de0ac09bfbcacbdf
Piotr <prusinski.pio@gmail.com>
2018-11-21 13:00:44 +0100

    implement breadth first search on word ladder example

--- chapter_7/02_breadth_first_search/word_ladder.py
+++ chapter_7/02_breadth_first_search/word_ladder.py
@@ -20,3 +20,31 @@ def buildGraph(wordFile):
                 if word1 != word2:
                     g.addEdge(word1, word2)
     return g
+
+
+def bfs(g, start):
+    start.setDistance(0)
+    start.setPred(None)
+    vertQueue = Queue()
+    vertQueue.enqueue(start)
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue()
+        for nbr in currentVert.getConnections():
+            if (nbr.getColor() == 'white'):
+                nbr.setColor('gray')
+                nbr.setDistance(currentVert.getDistance() + 1)
+                nbr.setPred(currentVert)
+                vertQueue.enqueue(nbr)
+        currentVert.setColor('black')
+
+def traverse(y):
+    x = y
+    while (x.getPred()):
+        print(x.getId())
+        x = x.getPred()
+    print(x.getId())
+
+
+wordGraph = buildGraph('wordfile.txt')
+bfs(wordGraph, wordGraph.getVertex('FOOL'))
+traverse(wordGraph.getVertex('SAGE'))
