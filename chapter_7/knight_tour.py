31a83c8759e504a1e7bc5871ef46b44848fc4c32
Piotr <prusinski.pio@gmail.com>
2018-11-21 13:48:10 +0100

    add legal moves generator for knight tour algorithm

--- chapter_7/knight_tour.py
+++ chapter_7/knight_tour.py
@@ -14,4 +14,20 @@ def knightGraph(bdSize):
 def posToNodeId(row, column, board_size):
     return (row * board_size) + column
 
+def genLegalMoves(x, y, bdSize):
+    newMoves = []
+    moveOffsets = [(-1,-2),(-1,2),(-2,-1),(-2,1),
+                   ( 1,-2),( 1,2),( 2,-1),( 2,1)]
+    for i in moveOffsets:
+        newX = x + i[0]
+        newY = y + i[1]
+        if legalCoord(newX, bdSize) and legalCoord(newY, bdSize):
+            newMoves.append((newX, newY))
+    return newMoves
 
+def legalCord(x, bdSize):
+    if x >= 0 and x < bdSize:
+        return True
+    else:
+        return False
+    
