da7a9621f2c60329c9a6ed1c6c0f3ba1f00ad85f
Piotr <prusinski.pio@gmail.com>
2018-11-21 11:53:02 +0100

    extend Vertex class, add wordfile.txt

--- chapter_7/01_graph/Vertex.py
+++ chapter_7/01_graph/Vertex.py
@@ -2,12 +2,47 @@ class Vertex:
     def __init__(self, key):
         self.id = key
         self.connectedTo = {}
+        self.color = 'white'
+        self.dist = sys.maxsize
+        self.pred = None
+        self.disc = 0
+        self.fin = 0
 
     def addNeighbour(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
 
-    def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+    def setColor(self, color):
+        self.color = color
+
+    def setDistance(self, d):
+        self.dist = d
+
+    def setPred(self, p):
+        self.pred = p
+
+    def setDiscovery(self, dtime):
+        self.disc = dtime
+
+    def setFinish(self, ftime):
+        self.fin = ftime
+
+    def getColor(self):
+        return self.color
+
+    def getDistance(self):
+        return self.dist
+
+    def getPred(self):
+        return self.pred
+
+    def getDiscovery(self):
+        return self.disc
+
+    def getFinish(self):
+        return self.fin
+        
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
 
     def getConnections(self):
         return self.connectedTo.keys()
@@ -18,3 +53,5 @@ class Vertex:
     def getWeight(self, nbr):
         return self.connectedTo[nbr]
 
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
