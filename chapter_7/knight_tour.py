961be24f6c9682130ac34b09a408376e433534e1
Piotr <prusinski.pio@gmail.com>
2018-12-05 18:54:40 +0100

    add knightTour algorithm main function, fix some typos in previous fuctions

--- chapter_7/knight_tour.py
+++ chapter_7/knight_tour.py
@@ -1,4 +1,4 @@
-from pythonds.graphs import graphs
+from pythonds.graphs import Graph, Vertex
 
 def knightGraph(bdSize):
     ktGraph = Graph()
@@ -25,9 +25,29 @@ def genLegalMoves(x, y, bdSize):
             newMoves.append((newX, newY))
     return newMoves
 
-def legalCord(x, bdSize):
+def legalCoord(x, bdSize):
     if x >= 0 and x < bdSize:
         return True
     else:
         return False
     
+def knightTour(n, path, u, limit):
+    u.setColor('gray')
+    path.append(u)
+    if n < limit:
+        nbrList = list(u.getConnections())
+        i = 0
+        done = False
+        while i < len(nbrList) and not done:
+            if nbrList[i].getColor() == 'white':
+                done = knightTour(n+1, path,nbrList[i], limit)
+            i = i + 1
+        if not done:
+            path.pop()
+            u.setColor('white')
+    else:
+        done = True
+    return Done
+
+
+knightGraph(64)
