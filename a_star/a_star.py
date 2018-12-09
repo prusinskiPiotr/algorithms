6ad2840d1650838f0e897447edb1e05e43770dbb
Piotr <prusinski.pio@gmail.com>
2018-12-09 14:11:18 +0100

    implement bread_first_search using redblobgames tutorial

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -5,5 +5,54 @@ steps that I need to take to implement a*:
 3. ???
 4. Profit
 """
+import collections
 
+
+class SimpleGraph:
+    def __init__(self):
+        self.edges = {}
+
+    def neighbours(self, index):
+        return self.edges[index]
+
+
+example_graph = SimpleGraph()
+example_graph.edges = {
+    'A': ['B'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A'],
+    'D': ['E', 'A'],
+    'E': ['B']
+}
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
+
+
+def breadth_first_search_1(graph, start):
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    while not frontier.empty():
+        current = frontier.get()
+        print("Visiting %r" % current)
+        for next in graph.neighbours(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
+
+
+breadth_first_search_1(example_graph, 'A')
 
