bcee319ae2e064801d997e2567d99516e86cf9e8
Piotr <prusinski.pio@gmail.com>
2018-12-09 15:18:05 +0100

    add grid drawing functions to breadth_first_search

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -16,6 +16,28 @@ class SimpleGraph:
         return self.edges[index]
 
 
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, index):
+        (x, y) = index
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, index):
+        return index not in self.walls
+
+    def neighbours(self, index):
+        (x, y) = index
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x + y) % 2 == 0: result.reverse()
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+
 example_graph = SimpleGraph()
 example_graph.edges = {
     'A': ['B'],
@@ -38,6 +60,31 @@ class Queue:
     def get(self):
         return self.elements.popleft()
 
+# utility functions for dealing with square grids
+def from_id_width(id, width):
+    return (id % width, id // width)
+
+def draw_tile(graph, id, style, width):
+    r = "."
+    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
+    if 'point_to' in style and style['point_to'].get(id, None) is not None:
+        (x1, y1) = id
+        (x2, y2) = style['point_to'][id]
+        if x2 == x1 + 1: r = ">"
+        if x2 == x1 - 1: r = "<"
+        if y2 == y1 + 1: r = "v"
+        if y2 == y1 - 1: r = "^"
+    if 'start' in style and id == style['start']: r = "A"
+    if 'goal' in style and id == style['goal']: r = "Z"
+    if 'path' in style and id in style['path']: r = "@"
+    if id in graph.walls: r = "#" * width
+    return r
+
+def draw_grid(graph, width=2, **style):
+    for y in range(graph.height):
+        for x in range(graph.width):
+            print("%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
+        print()
 
 def breadth_first_search_1(graph, start):
     frontier = Queue()
@@ -54,5 +101,9 @@ def breadth_first_search_1(graph, start):
                 visited[next] = True
 
 
-breadth_first_search_1(example_graph, 'A')
+# breadth_first_search_1(example_graph, 'A')
+DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+g = SquareGrid(30, 15)
+g.walls = DIAGRAM1_WALLS
+draw_grid(g)
 
