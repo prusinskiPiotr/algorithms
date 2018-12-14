85ff6dea8fc9d88724304891934854ad2ea81135
Piotr <prusinski.pio@gmail.com>
2018-12-14 18:26:14 +0100

    add BFS_3 which is early exit

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -117,11 +117,30 @@ def breadth_first_search_2(graph, start):
     
     return came_from
 
+def breadth_first_search_3(graph, start, goal):
+    frontier = Queue()
+    frontier.put(start)
+    came_from = {}
+    came_from[start] = None
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbours(current):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = current
+
+    return came_from
+
 # breadth_first_search_1(example_graph, 'A')
 DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 g = SquareGrid(30, 15)
 g.walls = DIAGRAM1_WALLS
 
-parents = breadth_first_search_2(g, (8, 7))
-draw_grid(g, width=2, point_to=parents, start=(8, 7))
+parents = breadth_first_search_3(g, (8, 7), (8, 10))
+draw_grid(g, width=2, point_to=parents, start=(8, 7), goal=(8, 10))
 
