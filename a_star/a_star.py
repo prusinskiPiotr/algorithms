ba1b736dcdc4c3ceb360096cc31d6e79620dfb40
Piotr <prusinski.pio@gmail.com>
2018-12-14 19:00:17 +0100

    add dijkstra algorithm

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -6,6 +6,7 @@ steps that I need to take to implement a*:
 4. Profit
 """
 import collections
+import heapq
 # from implementation import *
 
 
@@ -60,6 +61,30 @@ class Queue:
 
     def get(self):
         return self.elements.popleft()
+
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
 
 # utility functions for dealing with square grids
 def from_id_width(id, width):
@@ -136,11 +161,63 @@ def breadth_first_search_3(graph, start, goal):
 
     return came_from
 
-# breadth_first_search_1(example_graph, 'A')
-DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
-g = SquareGrid(30, 15)
-g.walls = DIAGRAM1_WALLS
+def dijkstra_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
 
-parents = breadth_first_search_3(g, (8, 7), (8, 10))
-draw_grid(g, width=2, point_to=parents, start=(8, 7), goal=(8, 10))
+    while not frontier.empty():
+        current = frontier.get()
 
+        if current == goal:
+            break
+
+        for next in graph.neighbours(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    return path
+
+diagram4 = GridWithWeights(10, 10)
+diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
+                                       (4, 3), (4, 4), (4, 5), (4, 6), 
+                                       (4, 7), (4, 8), (5, 1), (5, 2),
+                                       (5, 3), (5, 4), (5, 5), (5, 6), 
+                                       (5, 7), (5, 8), (6, 2), (6, 3), 
+                                       (6, 4), (6, 5), (6, 6), (6, 7), 
+                                       (7, 3), (7, 4), (7, 5)]}
+
+# # breadth_first_search_1(example_graph, 'A')
+# DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+# g = SquareGrid(30, 15)
+# g.walls = DIAGRAM1_WALLS
+
+# parents = breadth_first_searchcost_so_far or new_cost < cost_so_far[next]:
+# cost_so_far[next] = new_cost
+# priority = new_cost
+# frontier.put(next, priority)
+# came_from[next]_3(g, (8, 7), (8, 10))
+# draw_grid(g, width=2, point_to=parents, start=(8, 7), goal=(8, 10))
+
+came_from, cost_so_far = dijkstra_search(diagram4, (1, 4), (7, 8))
+draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+print()
+draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+print()
+draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
