4f23393ddd0fef2112d1332baa203eb26abc95b6
Piotr <prusinski.pio@gmail.com>
2018-12-17 17:28:01 +0100

    add redblobgames python a_star implementation

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -193,6 +193,35 @@ def reconstruct_path(came_from, start, goal):
         current = came_from[current]
     return path
 
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1-x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbours(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
 diagram4 = GridWithWeights(10, 10)
 diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
 diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
@@ -203,11 +232,13 @@ diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
                                        (6, 4), (6, 5), (6, 6), (6, 7), 
                                        (7, 3), (7, 4), (7, 5)]}
 
+# uncomment for BFS
 # # breadth_first_search_1(example_graph, 'A')
 # DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 # g = SquareGrid(30, 15)
 # g.walls = DIAGRAM1_WALLS
 
+# uncomment for early exit
 # parents = breadth_first_searchcost_so_far or new_cost < cost_so_far[next]:
 # cost_so_far[next] = new_cost
 # priority = new_cost
@@ -215,9 +246,19 @@ diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
 # came_from[next]_3(g, (8, 7), (8, 10))
 # draw_grid(g, width=2, point_to=parents, start=(8, 7), goal=(8, 10))
 
-came_from, cost_so_far = dijkstra_search(diagram4, (1, 4), (7, 8))
-draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+# uncomment for dijkstra
+# came_from, cost_so_far = dijkstra_search(diagram4, (1, 4), (7, 8))
+# draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+# print()
+# draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+# print()
+# draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
+
+
+# uncomment for a star
+start, goal = (1, 4), (7, 8)
+came_from, cost_so_far = a_star_search(diagram4, start, goal)
+draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
 print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+draw_grid(diagram4, width=3, number=cost_so_far, start=start, goal=goal)
 print()
-draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
