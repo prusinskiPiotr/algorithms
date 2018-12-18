9dedc476f4d0521041945d8bc0a3003279ca090c
Piotr <prusinski.pio@gmail.com>
2018-12-18 15:35:21 +0100

    add orderByAvail function to knight tour problem

--- chapter_7/knight_tour.py
+++ chapter_7/knight_tour.py
@@ -49,5 +49,17 @@ def knightTour(n, path, u, limit):
         done = True
     return Done
 
+def orderByAvail(n):
+    resList = []
+    for v in n.getConnections():
+        if v.getColor() == 'white':
+            c = 0
+            for w in v.getConnections():
+                if w.getColor() == 'white':
+                    c = c + 1
+            resList.append((c, v))
+    resList.sort(key=lambda x: x[0])
+    return [y[1] for y in resList]
+
 
 knightGraph(64)
