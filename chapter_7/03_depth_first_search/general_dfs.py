a7a0e0f3cb5b9ef4fd9e0c7aa9053e96d7c18e6f
Piotr <prusinski.pio@gmail.com>
2019-01-02 17:19:25 +0100

    implement general version of dfs algorithm

---
+++ chapter_7/03_depth_first_search/general_dfs.py
@@ -0,0 +1,28 @@
+from pythonds import Graph
+
+
+class DFSGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(-1)
+        for aVertex.getColor() == 'white':
+            self.dfsvisit(aVertex)
+
+    def dfsvisit(self, startVertex):
+        startVertex.setColor('gray')
+        self.time += 1
+        startVertex.setDiscovery(self.time)
+        for nextVertex.setDiscovery.getConnections():
+            if nextVertex.getColor == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex)
+        startVertex.setColor('black')
+        self.time += 1
+        startVertex.setFinish(self.time)
+        
+
