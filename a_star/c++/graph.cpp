262536307b381dc7d5404b540aa7742ba16f9e25
Piotr <prusinski.pio@gmail.com>
2019-01-09 19:46:18 +0100

    add c++ Vertex class, overload << operator for Vertex

--- a_star/c++/graph.cpp
+++ a_star/c++/graph.cpp
@@ -19,4 +19,32 @@ class Vertex{
 			connectedTo[nbr] = weight;
 		}
 
+		vector<int> getConnections(){
+			vector<int> keys;
+			// use the iterator to find all keys
+			for (map<int, int>::iterator it = connectedTo.begin();
+				it != connectedTo.end();
+				++it){
+					keys.push_back(it->first);
+			}
+			return keys;
+		}	
+
+		int getId(){
+			return id;
+		}
+
+		int getWeight(int nbr){
+			return connectedTo[nbr];
+		}
+
+		friend ostream &operator<<(ostream&, Vertex&);
 };
+
+ostream &operator<<(ostream &stream, Vertex &vert){
+	vector<int> connects = vert.getConnections();
+	for(unsigned int i = 0; i < connects.size(); i++){
+		stream << "( " << vert.id << " , " << connects[i] << " ) \n"; 
+	}
+	return stream;
+}
