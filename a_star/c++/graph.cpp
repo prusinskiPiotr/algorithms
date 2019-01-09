0f9f29286ba7789e5d73e114bd8cfdf34a67a760
Piotr <prusinski.pio@gmail.com>
2019-01-09 20:27:02 +0100

    add c++ Graph class

--- a_star/c++/graph.cpp
+++ a_star/c++/graph.cpp
@@ -48,3 +48,72 @@ ostream &operator<<(ostream &stream, Vertex &vert){
 	}
 	return stream;
 }
+
+
+class Graph{
+	public:
+		map<int, Vertex> vertList;
+		int numVertices;
+
+		Graph(){
+			numVertices = 0;
+		}
+
+		Vertex addVertex(int key){
+			numVertices++;
+			Vertex newVertex = Vertex(key);
+			this->vertList[key] = newVertex;
+			return newVertex;
+		}
+
+		Vertex *getVertex(int n){
+			for (map<int, Vertex>::iterator it = vertList.begin(); it != vertList.end(); ++it){
+				if (it->first == n){
+					// forced to use pointer due to possibility of returning NULL
+					Vertex *vpntr = &vertList[n];
+					return vpntr;
+				} else{
+					return NULL;
+				}
+			}
+		}
+
+		bool contains(int n){
+			for (map<int, Vertex>::iterator it = vertList.begin(); it != vertList.end(); ++it){
+				if (it->first == n){
+					return true;
+				}
+			}
+		}
+
+		void addEdge(int f, int t, int cost = 0){
+			if (!this->contains(f)){
+				cout << f << " was not found, adding!" << endl;
+				this->addVertex(f);
+			}
+			if (!this->contains(t)){
+				cout << t << " was not found, adding!" << endl;
+				this->addVertex(t);
+			}
+			vertList[f].addNeighbour(t, cost);
+		}
+
+		vector<int> getVertices() {
+			vector<int> verts;
+
+			for (map<int, Vertex>::iterator it = vertList.begin(); it != vertList.end(); ++it){
+				verts.push_back(it->first);
+			}
+			return verts;
+		}
+
+		friend ostream &operator<<(ostream&, Graph&);
+};
+
+ostream &operator<<(ostream &stream, Graph &grph){
+	for (unsigned int i = 0; i < grph.vertList.size(); i++){
+		stream << grph.vertList[i];
+	}
+
+	return stream;
+}
