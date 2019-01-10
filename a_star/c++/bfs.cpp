7b0d9472ce5ad40040d8a4691158c352674692e2
Piotr <prusinski.pio@gmail.com>
2019-01-10 20:57:04 +0100

    finish bfs c++ implementation

--- a_star/c++/bfs.cpp
+++ a_star/c++/bfs.cpp
@@ -184,7 +184,7 @@ Graph bfs(Graph g, Vertex *start){
         vertQueue.pop();
         for (unsigned int nbr = 0; nbr < currentVert->getConnections().size(); nbr++){
             if (g.vertList[currentVert->getConnections()[nbr]].color == 'w'){
-                g.vertList[currentVert->getConnections()[nbr]].color = 'w');
+                g.vertList[currentVert->getConnections()[nbr]].color = 'g';
 
                 g.vertList[currentVert->getConnections()[nbr]].dist = currentVert->dist + 1;
                 g.vertList[currentVert->getConnections()[nbr]].pred = currentVert;
@@ -196,3 +196,43 @@ Graph bfs(Graph g, Vertex *start){
     return g;
 }
 
+void traverse(Vertex *y){
+    Vertex *x = y;
+
+    while (x->pred){
+        cout << x->id << endl;
+        x = x->pred;
+    }
+    cout << x->id << endl;
+}
+
+int main() {
+    // Vector Initialized with an array
+    string arr[] = {"fool",
+                    "cool",
+                    "pool",
+                    "poll",
+                    "pole",
+                    "pall",
+                    "fall",
+                    "fail",
+                    "foil",
+                    "foul",
+                    "pope",
+                    "pale",
+                    "sale",
+                    "sage",
+                    "page"};
+
+    vector<string> words(arr, arr + (sizeof(arr) / sizeof(arr[0])));
+
+    // Graph g = buildGraph(words)
+    Graph g(false);
+    g = buildGraph(words);
+
+    g = bfs(g, g.getVertex("fool"));
+
+    traverse(g.getVertex("pall"));
+
+    return 0;
+}
