c5fcd6a52d948be3776c834c2280b770d50e2ec1
Piotr <prusinski.pio@gmail.com>
2019-01-15 23:47:57 +0100

    add bfs with square grid, working example with no obstacles

--- a_star/c++/wczytywanie.cpp
+++ a_star/c++/wczytywanie.cpp
@@ -5,48 +5,46 @@ using namespace std;
 
 int main (void) {
 
-cout<<"Wczytywanie danych z pliku\n";
+	cout<<"Wczytywanie danych z pliku\n";
 
-string nazwap="grid.txt";
-int wym2=20;
-int wym1=20;
+	string nazwap="grid.txt";
+	int wym2=20;
+	int wym1=20;
 
-//teraz deklarujemy dynamicznie tablice do, kt�rej wczytamyu nasz plik,
-int rows = wym2+1;
-double **G;
-G = new double*[rows];
-while(rows--) G[rows] = new double[wym1+1];
+	//teraz deklarujemy dynamicznie tablice do, kt�rej wczytamyu nasz plik,
+	int rows = wym2+1;
+	double **G;
+	G = new double*[rows];
+	while(rows--) G[rows] = new double[wym1+1];
 
-cout<<"\n\nNacisnij ENTER aby wczytac tablice o nazwie "<< nazwap;
-getchar();
+	cout<<"\n\nNacisnij ENTER aby wczytac tablice o nazwie "<< nazwap;
+	getchar();
 
-std::ifstream plik(nazwap.c_str());
-  
-for ( unsigned int i=1;i<wym2+1;i++)    
-  {
-    for ( unsigned int j=1;j<wym1+1;j++) 
-    {
-         plik >> G[i][j];
-    }
-  }  
-plik.close();
+	std::ifstream plik(nazwap.c_str());
 
-cout<<"\nWypisujemy na ekran\n\n";
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<G[i][j];
-   }cout<<"\n";
- }
-   
-//na koniec czy�cimy pami�� po naszej tablicy
-for(int i=0;i<wym2+1;i++)
-{delete[] G[i];}//czyscimy wiersze
-delete[] G;//zwalniamy tablice wskaznikow do wierszy
+	for ( unsigned int i=1;i<wym2+1;i++){
+		for ( unsigned int j=1;j<wym1+1;j++) {
+			plik >> G[i][j];
+		}
+	}  
+	plik.close();
 
-cout<<"\n\nNacisnij ENTER aby zakonczyc";
-getchar();
+	cout<<"\nWypisujemy na ekran\n\n";
+	for(int i=1;i<wym2+1;i++){
+		for(int j=1;j<wym1+1;j++){
+			cout<<" "<<G[i][j];
+		}
+		cout<<"\n";
+	}
 
-return 0;
+	//na koniec czy�cimy pami�� po naszej tablicy
+	for(int i=0;i<wym2+1;i++){
+		delete[] G[i];
+	}//czyscimy wiersze
+	delete[] G;//zwalniamy tablice wskaznikow do wierszy
+
+	cout<<"\n\nNacisnij ENTER aby zakonczyc";
+	getchar();
+
+	return 0;
 }
