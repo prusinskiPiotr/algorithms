e798907ddca5bfaa32418f106b16f514e9976c7e
Piotr Prusinski <prusinski.pio@gmail.com>
2019-10-15 15:18:53 +0200

    add classic and recursive binary search

---
+++ chapter_6/binary_search.py
@@ -0,0 +1,39 @@
+def binarySearch(alist, item):
+    first = 0
+    last = len(alist) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return found
+
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binarySearch(testlist, 3))
+print(binarySearch(testlist, 13))
+
+
+def recursiveBinarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist) // 2
+        if alist[midpoint] == item:
+            return True
+        else:
+            if item < alist[midpoint]:
+                return recursiveBinarySearch(alist[:midpoint], item)
+            else:
+                return recursiveBinarySearch(alist[midpoint+1:], item)
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(recursiveBinarySearch(testlist, 3))
+print(recursiveBinarySearch(testlist, 13))
