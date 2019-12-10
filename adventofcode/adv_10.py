85201dfb70597e4fa828df784ea375efa6b35485
Piotr Prusinski <prusinski.pio@gmail.com>
2019-12-10 12:11:18 +0100

    refactor advent of code intcode computer

---
+++ adventofcode/adv_10.py
@@ -0,0 +1,8 @@
+with open('data_10.txt') as file:
+    data = file.readlines()
+
+# x and y are inverted, meaning first bracket is y, second is x
+asteroid_map = [i.rstrip().split() for i in data]
+print(asteroid_map)
+
+def generate
